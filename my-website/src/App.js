5f757e437b4b915534fbf095222ba72bdfc67b24
Luke <103851112+moeshy@users.noreply.github.com>
2022-07-07 10:20:38 +0100

    basics

---
+++ my-website/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./Components/Header";
+
+function App() {
+  return (
+    <div id = "background">
+    <div>
+    <Header></Header>
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-website/src/Components/Header/index.js
@@ -0,0 +1,3 @@
+export default function Header(){
+    return <h1 id = "name">Luke Stewart</h1>
+}
